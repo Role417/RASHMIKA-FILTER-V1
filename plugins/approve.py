46bae43c92830bc16ce232ee7ff4efa5ef233742
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-14 14:33:43 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -12,7 +12,7 @@ async def accept_request(client, r):
     ]])
     
     try:
-        await client.send_message(            
+        await client.reply_text(            
             f"**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻\n\n 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title} 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽.\n\nSend /start to know more**",
             reply_markup=rm)
 
