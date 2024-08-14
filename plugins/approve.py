60f86341cf67b83e73ca1351824889a5e20fe326
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-14 15:25:01 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -12,9 +12,9 @@ async def accept_request(client, r):
     ]])
     
     try:
-        await client.send_text(
+        await bot.send_message(
             r.from_user.id,
-            text="**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻\n\n 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title} 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽.\n\nSend /start to know more**",
+            f"**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻\n\n 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title} 𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽.\n\nSend /start to know more**",
             reply_markup=rm)
                         
     except UserIsBlocked:
